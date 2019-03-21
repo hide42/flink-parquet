0ba089e78931a80d675b02d65997a9ff836cdf04
Peter <peter5566@yandex.ru>
2019-03-21 15:40:17 +0800

    minifix

--- src/test/java/KafkaSingleNodeComposeTest.java
+++ src/test/java/KafkaSingleNodeComposeTest.java
@@ -1,52 +1,37 @@
 import kafka.EmbeddedKafkaServer;
-import org.apache.commons.lang.RandomStringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
-
-import java.util.Properties;
 
 public class KafkaSingleNodeComposeTest {
 
     static EmbeddedKafkaServer kafkaServer = new EmbeddedKafkaServer();
 
-    private static final int countMessages = 100;
+    private static final int countMessages = 1000;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         String host = "localhost";
-        Integer port = kafkaServer.kbPort;
         kafkaServer.start();
         Thread.sleep(2000);
-        Thread thread = new Thread(){
+        kafkaServer.createTopic("test");
+
+        Thread thread = new Thread() {
             @Override
-            public void run(){
+            public void run() {
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Properties kafkaProps = new Properties();
-                kafkaProps.put("bootstrap.servers", host+":"+port);
-                kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-                kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-                KafkaProducer<String,String> producer = new KafkaProducer<String, String>(kafkaProps);
-
+                KafkaProducer<String,String> producer = kafkaServer.getProducer();
                 for (int i = 0; i < countMessages; i++) {
-                    producer.send(new ProducerRecord<String, String>("test", RandomStringUtils.random(10)));
+                    producer.send(new ProducerRecord<String, String>("test", "test_message_generator"));
                 }
             }
         };
         thread.start();
-        Thread thread2 = new Thread(){
-            @Override
-            public void run() {
-                Main.main(new String[]{host+":"+port, "test", "test_parquet"});
-            }
-        };
-        thread2.start();
-        Thread.sleep(5000);
-        thread2.interrupt();
-        thread2.stop();
+
+        //your main class with args
+        Main.main(new String[]{host + ":" + kafkaServer.kbPort,host+":"+kafkaServer.zkPort, "hdfs://localhost:9000/tmp/test_parquet"});
+
     }
 }
